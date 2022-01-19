a55a9370e442fb9264f3966dd9d15f83e1272146
horlando espinoza <orland20161@gmail.com>
2022-01-18 22:16:19 -0500

    se realizo ejercicio 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+    article = Article.new(
+        title: Faker::Movies::Hobbit.character,
+        content: Faker::Movies::Hobbit.quote
+    )
+    article.save!
+end
